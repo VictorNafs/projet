7cef729332c31e48e30ad718cad665ac16f92e3e
victor <duprezvictor4@gmail.com>
2023-05-11 18:51:04 +0200

    first commit

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,4 +4,12 @@ class SchedulesController < ApplicationController
     end
   end
   
+  class SchedulesController < ApplicationController
+    def day_schedule
+      @date = Date.parse(params[:date])
+      @reservations_by_hour = Reservation.where(start_time: @date.beginning_of_day..@date.end_of_day).group_by { |r| r.start_time.hour }
+    end
+    
+
+  end
   
