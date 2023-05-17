36a4007f7bd5dcd2fe69f9381e0e545500a03702
victor <duprezvictor4@gmail.com>
2023-05-18 01:07:34 +0200

    on va voir 6

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Solidapp
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.autoloader = :zeitwerk
 
     # Configuration for the application, engines, and railties goes here.
     #
