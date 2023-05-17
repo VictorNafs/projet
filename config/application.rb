570881009c8e4c0f3293697a23f7445131533b24
victor <duprezvictor4@gmail.com>
2023-05-18 01:10:35 +0200

    on va voir 7

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Solidapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.autoloader = :zeitwerk
+    config.eager_load_paths << Rails.root.join('app/models/spree/decorators')
 
     # Configuration for the application, engines, and railties goes here.
     #
