5f7df12902411f629b6e080d5bbc6e6d8adab4fc
victor <duprezvictor4@gmail.com>
2023-06-23 13:04:36 +0200

    first commit

--- app/mailers/store_mailer.rb
+++
@@ -1,7 +0,0 @@
-class StoreMailer < ApplicationMailer
-    def new_order_email(order, store_email)
-      @order = order
-      mail(to: store_email, subject: 'Nouvelle commande reçue')
-    end
-  end
-  
